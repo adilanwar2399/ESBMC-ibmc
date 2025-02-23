e8bcc9b5f640f5892cca91d2897cb911e8f0e535
adilanwar2399 <56471898+adilanwar2399@users.noreply.github.com>
2025-02-23 23:52:16 +0000

    Added Parsable Code2inv Benchmarks
    Added Code2inv Parsable Benchmarks - that are used in the Blackbox based pipelines.

---
+++ Benchmarks/ibmc_code2inv_parsable_benchmarks/24.c
@@ -0,0 +1,37 @@
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *); 
+void reach_error() { __assert_fail("0", "vnew2.c", 3, "reach_error"); }
+extern void abort(void);
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
+}
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: {reach_error();abort();}
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
+
+int abs(int x){
+  return x < 0 ? -x : x;
+}
+
+int main() {
+
+  int i = __VERIFIER_nondet_int();
+  int j = __VERIFIER_nondet_int();
+
+  (i = 1);
+  (j = 10);
+
+  while ((j >= i)) {
+    {
+    (i  = (i + 2));
+    (j  = (j - 1));
+    }
+
+  }
+
+__VERIFIER_assert( (j == 6) );
+}
---
+++ Benchmarks/ibmc_code2inv_parsable_benchmarks/33.c
@@ -0,0 +1,40 @@
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *); 
+void reach_error() { __assert_fail("0", "vnew2.c", 3, "reach_error"); }
+extern void abort(void);
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
+}
+void __VERIFIER_assert(int cond) {
+  if (!(cond)) {
+    ERROR: {reach_error();abort();}
+  }
+  return;
+}
+int __VERIFIER_nondet_int();
+
+int abs(int x){
+  return x < 0 ? -x : x;
+}
+
+int main() {
+
+  int n = __VERIFIER_nondet_int();
+  int v1 = __VERIFIER_nondet_int();
+  int v2 = __VERIFIER_nondet_int();
+  int v3 = __VERIFIER_nondet_int();
+  int x = __VERIFIER_nondet_int();
+
+  (x = n);
+
+  while ((x > 0)) {
+    {
+    (x  = (x - 1));
+    }
+
+  }
+
+if ( (x != 0) )
+__VERIFIER_assert( (n < 0) );
+
+}
